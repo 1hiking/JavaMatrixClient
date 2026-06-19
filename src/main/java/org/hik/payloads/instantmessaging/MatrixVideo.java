d95065c05dde23ffa51d5766e2fe8c910aeaed64
J.C <73438877+1hiking@users.noreply.github.com.>
2026-06-18 23:14:27 -0600

    Add matrix video

---
+++ src/main/java/org/hik/payloads/instantmessaging/MatrixVideo.java
@@ -0,0 +1,48 @@
+package org.hik.payloads.instantmessaging;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.net.URI;
+
+/**
+ *
+ * This message represents a single video clip.
+ *
+ * @param body          the filename of the original upload if {@code filename} is unset
+ *                      or identical to it; otherwise, a caption for the video.
+ * @param file          information on the encrypted file, as specified in End-to-end
+ *                      encryption. Required if the file is encrypted.
+ * @param filename      the original filename of the uploaded file.
+ * @param format        the format used in {@code formattedBody}. Required if
+ *                      {@code formattedBody} is specified; currently only
+ *                      {@code org.matrix.custom.html} is supported.
+ * @param formattedBody the formatted version of {@code body}, when it acts as a caption.
+ *                      Required if {@code format} is specified.
+ * @param info          metadata for the audio clip referred to by {@code url}.
+ * @param msgtype       always {@value #TYPE}.
+ * @param url           required if the file is unencrypted.
+ */
+public record MatrixVideo(String body,
+                          EncryptedFile file,
+                          String filename,
+                          String format,
+                          @JsonProperty("formatted_body") String formattedBody,
+                          VideoInfo info,
+                          String msgtype,
+                          URI url
+) implements MatrixEvent {
+
+    public static final String TYPE = "m.video";
+
+
+    record VideoInfo(Integer duration,
+                     Integer h,
+                     String mimetype,
+                     Integer size,
+                     @JsonProperty("thumbnail_file") EncryptedFile thumbnailFile,
+                     @JsonProperty("thumbnail_info") ThumbnailInfo thumbnailInfo,
+                     @JsonProperty("thumbnail_url") String thumbnailUrl,
+                     Integer w
+    ) implements HasThumbnail, HasInfo {
+    }
+}
