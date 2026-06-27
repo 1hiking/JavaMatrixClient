ad2970c881ce3456d7ad140abdd427dde0fd9c11
J.C <73438877+1hiking@users.noreply.github.com.>
2026-06-27 17:52:50 -0600

    Fix matrix event serialization

--- src/main/java/org/hik/payloads/roomevents/MatrixEvent.java
+++ src/main/java/org/hik/payloads/roomevents/MatrixEvent.java
@@ -1,19 +1,14 @@
 package org.hik.payloads.roomevents;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /// The interface that all matrix m.room.events have to inherit.
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "msgtype",
-        visible = true
-)
 @JsonInclude(JsonInclude.Include.NON_ABSENT)
 public sealed interface MatrixEvent
         permits MatrixAudio, MatrixFile, MatrixLocation, MatrixImage, MatrixText, MatrixVideo {
     /// @return the event type represented with a "m." prefix.
+    @JsonProperty("msgtype")
     String msgtype();
 
     /// @return depending on the event it can either be an url mxc:// or a text to show.
